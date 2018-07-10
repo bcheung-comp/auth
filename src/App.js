199d22978bd92f98a102ef7c1be3939166e1b7ad
b-cheung <bcheung2017@gmail.com>
2018-07-09 20:35:09 -0500

    eslint setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
-import firebase from "firebase";
-import { Header } from "./components/common";
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: "AIzaSyD2IFB87-dPE9_p3nLceXUGQ7xD_N4_Vzk",
-      authDomain: "auth-comp.firebaseapp.com",
-      databaseURL: "https://auth-comp.firebaseio.com",
-      projectId: "auth-comp",
-      storageBucket: "auth-comp.appspot.com",
-      messagingSenderId: "500798647963"
+      apiKey: 'AIzaSyD2IFB87-dPE9_p3nLceXUGQ7xD_N4_Vzk',
+      authDomain: 'auth-comp.firebaseapp.com',
+      databaseURL: 'https://auth-comp.firebaseio.com',
+      projectId: 'auth-comp',
+      storageBucket: 'auth-comp.appspot.com',
+      messagingSenderId: '500798647963'
     });
   }
   render() {
